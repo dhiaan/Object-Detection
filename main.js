e74e48bd52803a8bec2278942810239a84b9f7dd
dhiaan <82027019+dhiaan@users.noreply.github.com>
2021-11-30 17:43:48 +0845

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function setup() {
- canvas = createCanvas(640, 420)
+ canvas = createCanvas(380, 380);
  canvas.center();
+ video = createCapture(VIDEO);
+ video.size(380,380);
+ video.hide();
  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
  document.getElementById("status").innerHTML = "Status Detecting Object";
 }
@@ -13,19 +16,23 @@ img = loadImage('dog_cat.jpg');
 }
 
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
 
     if(status != "")
     {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
         for(i = 0; 1 < objects.length;i++)
         {
             document.getElementById("status").innerHTML = "Status : Object Detected";
-
-            fill("#FF0000");
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+            fill(r,g,b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%" , objects[i].x, objects[i].y);
             noFill();
-            stroke("#FF0000");
+            stroke("r,g,b");
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 
         }
@@ -36,7 +43,7 @@ function draw() {
 function modelLoaded() {
     console.log("Model Loded!")
     status = true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results) {
